db88d92ba62b1a21e4b9503356ba5a351bbfdfce
KEVIN RAMIREZ <kevin.ramirez1996.gmail.com>
2016-10-20 17:21:42 -0500

    Fix upload of image with detect face

--- public/app/js/front/controllers/mainControllers.js
+++ public/app/js/front/controllers/mainControllers.js
@@ -86,43 +86,54 @@ angular.module('load.mainControllers', [])
         if(format && !size){
             alert('El archivo '+ item.name +' no debe sobre pasar las 5MB');
         }
-    };
+    }
+
     uploader.onAfterAddingFile = function(fileItem) {
         console.info('onAfterAddingFile', fileItem);
         fileItem.formData.push({name: fileItem.file.name});
-    };
+    }
+
     uploader.onAfterAddingAll = function(addedFileItems) {
         console.info('onAfterAddingAll', addedFileItems);
-    };
+    }
+
     uploader.onBeforeUploadItem = function(item) {
         console.info('onBeforeUploadItem', item);
-    };
+    }
+
     uploader.onProgressItem = function(fileItem, progress) {
         console.info('onProgressItem', fileItem, progress);
-    };
+    }
+
     uploader.onProgressAll = function(progress) {
         console.info('onProgressAll', progress);
-    };
+    }
+
     uploader.onSuccessItem = function(fileItem, response, status, headers) {
         console.info('onSuccessItem', fileItem, response, status, headers);
+        console.info('onSuccessItem@response', response);
         if(response['success'] === true){ 
             if(angular.isUndefined(response['attributes'])){
-                console.log('onSuccessItem@content', response['attributes']); 
+                console.log('onSuccessItem@attributes', response['attributes']); 
             }
         }
-    };
+    }
+
     uploader.onErrorItem = function(fileItem, response, status, headers) {
         console.info('onErrorItem', fileItem, response, status, headers);
-    };
+    }
+
     uploader.onCancelItem = function(fileItem, response, status, headers) {
        console.info('onCancelItem', fileItem, response, status, headers);
-    };
+    }
+
     uploader.onCompleteItem = function(fileItem, response, status, headers) {
         console.info('onCompleteItem', fileItem, response, status, headers);
-    };
+    }
+
     uploader.onCompleteAll = function() {
         console.info('onCompleteAll');
-    };
+    }
 
     console.info('uploader', uploader);
 }]);
